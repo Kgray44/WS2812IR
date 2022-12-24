4d112ab48f06cfb114b72d93c43890055dbbb11e
K Gray <64373011+Kgray44@users.noreply.github.com>
2022-12-24 07:28:40 -0500

    Add Info

--- Code Collection/WS2812IR_No_Blocking/WS2812IR_No_Blocking.ino
+++ Code Collection/WS2812IR_No_Blocking/WS2812IR_No_Blocking.ino
@@ -1,3 +1,30 @@
+/*************************************************************
+File Name: WS2812IR_No_Blocking.ino
+Processor/Platform: Arduino (Nano tested)
+Development Environment: Arduino 2.0.13
+
+Download latest code here:
+https://github.com/Kgray44/WS2812IR
+
+Driveway Gates code meant to be used alongside the tutorial found here:
+https://www.hackster.io/k-gray/how-to-use-ir-remote-and-ws2812-leds-with-arduino-342df9
+
+Copyright 2022 K Gray
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Liscense found here:
+https://opensource.org/licenses/MIT
+ *************************************************************/
+
 bool testIR = false;//true or false
 
 #include <IRremote.h>
